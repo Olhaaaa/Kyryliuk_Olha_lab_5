6b70592203f8a2fe415689b6af1afd1f323bb1eb
Olhaaaa <oliakyryliuk04@gmail.com>
2022-06-16 10:44:17 +0300

    Labs(OOP term2)

---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TansportType;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Depo depo = new Depo();
+
+            depo.Add("train", 60, 2);
+            depo.Add("bus", 80, 3);
+            depo.Add("helicopter", 180, 4);
+            depo.Add("car", 100, 4);
+            depo.Add("airplane", 900, 5);
+            depo.Choose();  
+
+        }
+    }
+}
---
+++ Lab_4/Lab_4/Transport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab_4
+{
+    abstract class Transport
+    {
+        public double speed;     
+        public double cost;       
+        public double length;
+        public Transport(double speed, double cost)
+        {
+            this.speed = speed;
+            this.cost = cost;
+        }
+        public void Length()
+        {
+            Console.WriteLine("Введіть довжину маршруту:");
+            length = Convert.ToDouble(Console.ReadLine());            
+        }
+        public void Price()
+        {
+            Console.WriteLine($"Вартість поїздки: {cost * length}");
+        }
+        public void Time()
+        {            
+            Console.WriteLine($"Час подорожі:{Math.Round(length/speed,2)} год. ") ;
+        }
+
+    }
+}

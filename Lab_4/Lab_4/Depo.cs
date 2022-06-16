d194696f62c5b443efb9c021627e95181496937b
Olhaaaa <oliakyryliuk04@gmail.com>
2022-06-16 10:26:10 +0300

    Labs(OOP term 2)

---
+++ Lab_4/Lab_4/Depo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TansportType;
+
+namespace Lab_4
+{
+    class Depo
+    {
+       
+        public List < Transport> transport = new List< Transport>();
+
+        public void Add( string name, double speed, double cost)
+        {
+            if(name=="train")
+            {
+                transport.Add(new Train(speed, cost));
+            }
+            else if (name =="airplane")
+            {
+                transport.Add(new Airplane(speed, cost));
+            }
+            else if (name=="car")
+            {
+                transport.Add(new Car(speed, cost));
+            }
+            else if (name =="bus")
+            {
+                transport.Add(new Bus (speed, cost));
+            }
+            else
+            {
+                transport.Add(new Helicopter(speed, cost));
+            }
+        }
+
+        public void Choose( )
+        {
+            Console.WriteLine("Оберіть вид транспорту: ");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "train":
+                    transport[0].Length();
+                    transport[0].Price();
+                    transport[0].Time();
+                    break;
+
+                case "bus":
+                    transport[1].Length();
+                    transport[1].Price();
+                    transport[1].Time();
+                    break;
+                case "car":
+                    transport[3].Length();
+                    transport[3].Price();
+                    transport[3].Time();
+                    break;
+                case "airplane":
+                    transport[4].Length();
+                    transport[4].Price();
+                    transport[4].Time();
+                    break;
+                case "helicopter":
+                    transport[2].Length();
+                    transport[2].Price();
+                    transport[2].Time();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Lab_4/Lab_4/TransportType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab_4;
+
+
+namespace TansportType
+{
+    class Airplane : Transport
+    {
+        static public string name = "airplane";
+        public Airplane(double speed, double cost) : base(speed, cost) { }
+
+    }
+
+    class Train : Transport
+    {
+        static public string name = "train";
+        public Train (double speed, double cost) : base(speed, cost) { }
+
+    }
+    class Car : Transport
+    {
+        static public string name = "car";
+        public Car (double speed, double cost) : base(speed, cost) { }
+    }
+
+    class Bus : Transport
+    {
+        static public string name = "bus";
+        public Bus(double speed, double cost) : base(speed, cost) { }
+    }
+    class Helicopter : Transport 
+    {
+        static public string name = "helicopter";
+        public Helicopter(double speed, double cost) : base(speed, cost) { }
+    }
+
+
+}
